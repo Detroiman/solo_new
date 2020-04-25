8b51af15cf47d259ce20136dc196341fda81c1fe
Detroiman <52424452+Detroiman@users.noreply.github.com>
2020-04-25 12:41:49 +0300

    Magazine

--- Assets/Scripts/Menu/Magazine/magazinecontroll.cs
+++ Assets/Scripts/Menu/Magazine/magazinecontroll.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class magazinecontroll : MonoBehaviour
 {
     public GameObject back;
+	public GameObject Abilities;
+	public GameObject Guns;
+	public GameObject Other;
     void Start()
     {
         
@@ -18,5 +21,17 @@ public class magazinecontroll : MonoBehaviour
 	public void buttonBack()
 	{
 		Application.LoadLevel("Main Menu");
+	}
+	public void buttonAbilities()
+	{
+		Application.LoadLevel("Abilities_magazine");
+	}
+	public void buttonOther()
+	{
+		Application.LoadLevel("Other_magazine");
+	}
+	public void buttonGuns()
+	{
+		Application.LoadLevel("Guns_magazine");
 	}
 }
